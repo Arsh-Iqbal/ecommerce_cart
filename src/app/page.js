f24abca0fec22e67e590fc4612197de25f7966d2
Arsh <iqbalarsh54@gmail.com>
2025-06-15 00:40:28 +0530

    creaate home page with filter and product card

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,32 @@
-"use client";
+'use client'
 
-import ProductCard from "../components/ProductCard";
-import Filters from "../components/Filters";
-import productsData from "../data/products.json";
-import { useEffect, useState } from "react";
+import ProductCard from '../components/ProductCard'
+import Filters from '../components/Filters'
+import productsData from '../data/products.json'
+import { useEffect, useState } from 'react'
 
 export default function HomePage() {
-  const [filters, setFilters] = useState({ category: "All", price: 1000 });
-  const [filteredProducts, setFilteredProducts] = useState(productsData);
+  const [filters, setFilters] = useState({ category: 'All', price: 1000 })
+  const [filteredProducts, setFilteredProducts] = useState(productsData)
 
   useEffect(() => {
-    let filtered = productsData;
+    let filtered = productsData
 
     // Category filter
-    if (filters.category !== "All") {
-      filtered = filtered.filter((p) => p.category === filters.category);
+    if (filters.category !== 'All') {
+      filtered = filtered.filter((p) => p.category === filters.category)
     }
 
     // Price filter
     filtered = filtered.filter((p) => p.price <= filters.price);
 
-    setFilteredProducts(filtered);
-  }, [filters]);
+    setFilteredProducts(filtered)
+  }, [filters])
 
   return (
     <div className="min-h-screen flex flex-col">
+    
+
       <main className="flex flex-1 max-w-7xl mx-auto w-full p-4 gap-6">
         {/* Sidebar */}
         <aside className="w-full md:w-64">
@@ -42,6 +44,11 @@ export default function HomePage() {
           )}
         </section>
       </main>
+
+   
     </div>
-  );
+  )
+
+
 }
+
