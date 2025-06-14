e058763619ffa617ca49ff45fd994e3f4a881f20
Arsh <iqbalarsh54@gmail.com>
2025-06-15 00:57:45 +0530

    create context for managing cart state

---
+++ src/context/CartContext.js
@@ -0,0 +1,44 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState(() => {
+    if (typeof window !== "undefined") {
+      const stored = localStorage.getItem("cart");
+      return stored ? JSON.parse(stored) : [];
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (product, quantity = 1) => {
+    setCart((prev) => {
+      const existing = prev.find((item) => item.id === product.id);
+      let newCart;
+      if (existing) {
+        newCart = prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + quantity }
+            : item
+        );
+      } else {
+        newCart = [...prev, { ...product, quantity }];
+      }
+
+      return newCart;
+    });
+  };
+  return (
+    <CartContext.Provider value={{ cart, setCart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
