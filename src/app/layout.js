f01901858866f46be890102c056ac739f8fd48f9
Arsh <iqbalarsh54@gmail.com>
2025-06-15 01:22:37 +0530

    wrap with cartprovider and toaster

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 
+import { CartProvider } from '@/context/CartContext'
 import './globals.css'
 import Header from '@/components/Header'
+import { Toaster } from 'react-hot-toast'
 
 export const metadata = {
   title: 'E-Commerce App',
@@ -11,10 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-      
-          <Header/>
-         
+        <CartProvider>
+           <Header />
           <main>{children}</main>
+            <Toaster position="top-right" />
+        </CartProvider>
        
       </body>
     </html>
