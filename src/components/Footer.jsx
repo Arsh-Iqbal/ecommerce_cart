919394b7f8a18b7dfa380c65822d18d6664bc856
Arsh <iqbalarsh54@gmail.com>
2025-06-15 17:06:32 +0530

    Footer part done with responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,10 @@ export default function Footer() {
   return (
     <footer className="bg-blue-700 text-white py-6 mt-10">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-        <p className="text-sm">© 2025 All rights reserved. eZtronik</p>
+        <p className="text-sm">© 2025 All rights reserved. Arsh</p>
+      
         <div className="flex gap-4 mt-2 md:mt-0">
+            <p className='text-sm'>Contact US</p>
           <Facebook className="hover:text-blue-300 cursor-pointer" />
           <Twitter className="hover:text-blue-300 cursor-pointer" />
           <Instagram className="hover:text-blue-300 cursor-pointer" />
